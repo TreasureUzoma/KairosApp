330e343374275ffb75f7c13245b36c78f7eec240
Treasure Uzoma <123127790+TreasureUzoma@users.noreply.github.com>
2025-04-06 12:41:56 +0000

    auth logig change

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,7 +1,6 @@
 /* eslint-disable */
 
 import { db } from "@/lib/firebase";
-import type { NextAuthOptions } from "next-auth";
 import NextAuth from "next-auth";
 import type { DefaultSession } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
