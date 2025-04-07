cc4c8f2d9885253dbf0a09a91c0d961a41a94a48
TreasureUzoma <treasureuzoma650@gmail.com>
2025-04-07 01:07:40 +0100

    gn

--- components/CreateAccountButton.tsx
+++ components/CreateAccountButton.tsx
@@ -1,14 +1,19 @@
 import { auth } from "@/lib/auth";
 import Link from "next/link";
 
-export default async function CreateAccountButton = () => {
+export default async function CreateAccountButton() {
     const session = await auth();
 
     if (session?.user) return null;
     // returns nothing since user is already logged in
 
     return (
-        <Link className="px-3 py-2 border border-black rounded-sm bg-white text-black dark:border-white bg-black text-black hover: opacity-90" href="/auth">Create account </Link>
+        <Link
+            className="px-3 py-2 border border-black rounded-sm bg-white text-black dark:border-white bg-black text-black hover: opacity-90"
+            href="/auth"
+        >
+            Create account
+        </Link>
     );
-};
+}
 export default CreateAccountButton;
